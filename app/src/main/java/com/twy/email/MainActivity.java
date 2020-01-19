1fc46bde1e722616c814b7e5ceacb87c49a16bb9
tuwenyuan <123456>
2020-01-19 15:48:12 +0800

    init project

--- app/src/main/java/com/twy/email/MainActivity.java
+++ app/src/main/java/com/twy/email/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 SendMailUtil.send();
+
             }
         });
     }
